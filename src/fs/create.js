da7f607e5fd40ae2726df3c8f503bde74819dc52
anksuunamun <wgorelova@gmail.com>
2024-01-30 00:36:54 +0300

    feat: implement create file function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,16 @@
+import { access, appendFile, constants } from "node:fs/promises";
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await access("src/fs/files/fresh.txt", constants.F_OK);
+    throw new Error("FS operation failed");
+  } catch (e) {
+    if (e.code === "ENOENT") {
+      return await appendFile("src/fs/files/fresh.txt", "I am fresh and young");
+    }
+
+    throw new Error("FS operation failed");
+  }
 };
 
 await create();
